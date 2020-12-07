a5eeabfba0282faa12264ac3d150b29c07b61bdb
Trey Davis <trey@jwdavis.tech>
2020-12-07 12:06:41 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Trey Davis on 12/7/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
